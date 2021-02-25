84d3ea2d0e28bc9d7feaf8d47b873e17f7c118c0
ElenaAndronache <andronacheelena18@stud.ase.ro>
2021-02-25 13:32:32 +0200

    Initial commit

---
+++ seminar1/src/Clase/Zebra.java
@@ -0,0 +1,20 @@
+package Clase;
+
+public class Zebra extends Animal {
+	
+	private int greutate;
+
+	public Zebra(String name, int greutate) {
+		super(name);
+				this.greutate = greutate;
+	}
+
+	public Zebra(String name) {
+		super(name);
+		this.greutate = greutate;
+	}
+	
+	
+	
+
+}
--- seminar1/src/pachet/main.java
+++ seminar1/src/pachet/main.java
@@ -2,6 +2,7 @@ package pachet;
 
 import Clase.Zoo;
 import Clase.Giraffe;
+import Clase.Zebra;
 
 public class main {
 	
@@ -10,8 +11,13 @@ public class main {
 		Zoo zoo=new Zoo();
 		Giraffe g1= new Giraffe("Gifi",300);
 		Giraffe g2=new Giraffe("Lola");
+		Zebra z1=new Zebra("zebri",100);
+		Zebra z2=new Zebra("zebriiiii");
 		zoo.addAnimal(g1);
 		zoo.addAnimal(g2);
+		
+		zoo.addAnimal(z1);
+		zoo.addAnimal(z2);
 		
 		zoo.feedAllAnimals();
 	}

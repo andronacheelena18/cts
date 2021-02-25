23310d3e2303e8cbad7c2f22a58211cf6bb433ef
ElenaAndronache <andronacheelena18@stud.ase.ro>
2021-02-25 13:25:34 +0200

    Initial commit

--- seminar1/src/pachet/main.java
+++ seminar1/src/pachet/main.java
@@ -1,16 +1,19 @@
 package pachet;
 
 import Clase.Zoo;
+import Clase.Giraffe;
 
 public class main {
 	
 	public static void main(String[] args) {
 	
 		Zoo zoo=new Zoo();
-		Giraffe g1= new Girrafe("Gifi",300);
+		Giraffe g1= new Giraffe("Gifi",300);
 		Giraffe g2=new Giraffe("Lola");
 		zoo.addAnimal(g1);
 		zoo.addAnimal(g2);
+		
+		zoo.feedAllAnimals();
 	}
 
 }

dba41cb26bab09a40ebee0897fbb74815ce26812
ElenaAndronache <andronacheelena18@stud.ase.ro>
2021-03-04 12:31:21 +0200

    Initial Commit

--- seminar 2/src/Ro/ase/cts/program/Program.java
+++ seminar 2/src/Ro/ase/cts/program/Program.java
@@ -1,7 +1,9 @@
 package Ro.ase.cts.program;
  import Ro.ase.cts.Clase.Angajat;
 import Ro.ase.cts.Clase.Utils;
+import Ro.ase.cts.Clase.Aplicant;
 import Ro.ase.cts.program.*;
+import Ro.ase.cts.Clase.readers.*;
 
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -11,11 +13,11 @@ import java.util.List;
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAplicanti;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			listaAplicanti = EmployeeReader.readAngajati("angajati.txt");
+			for(Aplicant aplicant:listaAplicanti)
+				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
